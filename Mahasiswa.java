8bde35e1efc29f8bd29039f1498199845ab9effc
Nanda Nabilatul Aeni <bilaaeni34@gmail.com>
2025-10-01 14:20:39 +0700

    Code Encapsulation

---
+++ Mahasiswa.java
@@ -0,0 +1,20 @@
+public class Mahasiswa {
+    private String nama;
+    private int umur;
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getNama() {
+        return this.nama;
+    }
+
+    public void setUmur(int umur) {
+        this.umur = umur;
+    }
+
+    public int getUmur() {
+        return this.umur;
+    }
+}
